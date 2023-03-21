882de3174f0613904d3972d03881fb21a7d50800
yflop0 <125532795+yflop0@users.noreply.github.com>
2023-03-21 22:57:33 +0800

    Update and rename Q2 to Q2.py

---
+++ Q2.py
@@ -0,0 +1,13 @@
+#Q2
+x = int(input()) 
+y = int(input()) 
+
+#int converts the string value into an integer
+#input is the input by the user
+
+if x > y: 
+  result = x - y 
+else: 
+  result = y - x
+  
+print(result)
