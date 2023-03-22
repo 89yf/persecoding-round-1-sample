e283ba3a300fe45d46eed5a5d68021c888450617
yflop0 <125532795+yflop0@users.noreply.github.com>
2023-03-22 16:55:06 +0800

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,7 +1,20 @@
 #Q3
+
+word = input()
+result = word
+
+while len(result) <= 30:       #while loop continues to execute the code repeatedly as long as the condition is true
+    result += word             #word += word is the same as word = word + word
+
+print(result)  
+
+
+    #OR
+
+    
 word = input().strip()
 
 while len(word) <= 30:
-    word += word              #word += word is the same as word = word + word
+    word += word
 
-print(word[:30])              #prints the 30 characters of the word
+print(word[:30])                #prints the 30 characters of the word
